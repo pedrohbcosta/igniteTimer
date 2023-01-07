b4d13e89107d20bf7c0fe9e809a229e875b55bb0
Pedro Batista <pedro@yellowberry.com.br>
2023-01-07 20:52:04 -0300

    Started a new project of a timer your Rocketseat

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom'
+
+import { HeaderContainer } from "./styles";
+
+import LogoIgnite from "../../assets/LogoIgnite.svg";
+import { Timer, Scroll } from 'phosphor-react'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={ LogoIgnite } alt="" />
+      <nav>
+        <NavLink to="/" title='Timer'> 
+          <Timer size={24}/>
+        </NavLink>
+        
+        <NavLink to="/history" title='Histórico'>
+          <Scroll size={24}/>
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
